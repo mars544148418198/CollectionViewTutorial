06b5bb412698e3d05aceca883be5858c61fee271
User <User@WIN-C9HQIV4VA1U>
2021-12-07 22:30:13 +0300

    Add project files.

---
+++ CollectionViewTutorial/CollectionViewTutorial/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using CollectionViewTutorial.ViewModels;
+using CollectionViewTutorial.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace CollectionViewTutorial
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
